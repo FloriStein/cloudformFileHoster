343a039c87b8911941c2c295f514f47aa06a0de9
Cett <florian_steinmann@hotmail.de>
2025-02-22 10:21:01 +0100

    filehoster frontend componentens added

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,13 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { Authenticator } from "@aws-amplify/ui-vue";
+import "@aws-amplify/ui-vue/styles.css";
+import FileManager from './components/FileManager.vue';
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <authenticator>
+    <template v-slot="{ signOut }">
+      <FileManager :signOut="signOut" />
+    </template>
+  </authenticator>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
---
+++ src/types/types.ts
@@ -0,0 +1,7 @@
+export interface FileItem {
+  name: string;
+  path: string;
+  size?: number;
+  uploadedAt?: string;
+  url?: string;
+}
